e47935fca3f878a4b80d1140964415bc2093f41c
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-01-10 14:13:16 -0800

    Setup environment

---
+++ spec/paragraph.js
@@ -0,0 +1,16 @@
+import chai, { expect } from 'chai'
+
+const fs = require('fs')
+const singleLine = fs.readFileSync('src/paragraph/singleLine.md', 'utf8')
+
+describe('paragraph', () => {
+
+  context('object-oriented', () =>{
+
+    // it('wraps single line in <p> tag', () => {
+    //   expect(ooParser.outputMarkdown())
+    //     .to.equal('<p>Crying is beautiful.</p>')
+    // })
+  })
+
+})
